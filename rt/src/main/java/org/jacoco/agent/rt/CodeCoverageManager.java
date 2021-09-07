ed559136cd676ff74384e84f86c4e23ca82d1a73
Dusan <duqian2010@gmail.com>
2021-09-07 11:23:03 +0800

    update host address

--- rt/src/main/java/org/jacoco/agent/rt/CodeCoverageManager.java
+++ rt/src/main/java/org/jacoco/agent/rt/CodeCoverageManager.java
@@ -29,7 +29,8 @@ import okhttp3.Response;
  */
 public class CodeCoverageManager {
     private static CodeCoverageManager sInstance=new CodeCoverageManager();
-    private static String URL_HOST = "http://10.10.17.105:8080";//内网 服务器地址
+    //private static String URL_HOST = "http://10.10.17.105:8080";//内网 服务器地址
+    private static String URL_HOST = "http://localhost:8080";//内网 服务器地址
 
     private static String APP_NAME;
     private static int versionCode;
